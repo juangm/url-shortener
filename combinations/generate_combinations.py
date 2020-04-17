f054952d2ec932ab039be021204eb46221efb34d
juangm <juangarciamarinblog@gmail.com>
2020-04-17 14:40:43 +0200

    Add tool to generate all possible combinations

---
+++ combinations/generate_combinations.py
@@ -0,0 +1,13 @@
+import itertools
+import json
+import string
+
+# Create the string with all possible charactesr according to specs
+charset = string.ascii_lowercase + string.digits + string.ascii_uppercase
+
+# Calculate all the combinations and convert to a list
+list_combinations = list(itertools.product(charset, repeat=2))
+
+# Write all the combination to a json file for later use it
+with open('./combinations/encoding.json', 'w', encoding='utf-8') as f:
+    json.dump(list_combinations, f, ensure_ascii=False, indent=2)
