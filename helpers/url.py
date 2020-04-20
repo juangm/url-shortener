9ad76b4dd7610ec068024bb5db39caaad09ca881
juangm <juangarciamarinblog@gmail.com>
2020-04-20 14:33:03 +0200

    Refactor url helpers

--- helpers/url.py
+++ helpers/url.py
@@ -1,24 +1,37 @@
 from urllib.parse import urlparse
+
+import json
 import validators
 
 
 class UrlHelper:
 
     def __init__(self):
+        self.base_domain = 'www.applau.se'
+        self.combinations = None
         # Load the possible combinations
-        self.base_domain = 'https://www.applau.se/'
-        self.combinations = []
+        with open('combinations/encoding.json') as f:
+            self.combinations = json.load(f)
 
-    def get_domain(self, input_url):
-        """Method to get the domain of the url"""
+    def check_domain(self, input_url):
+        """Method to detect if url needs to be shorted or return original"""
         # Check the input is a valid URL
         if validators.url(input_url):
-            return urlparse(input_url).netloc
+            url_parsed = urlparse(input_url)
+            if self.base_domain in url_parsed.netloc:
+                # Check path doesn't contains more than 2 characters
+                if len(url_parsed.path) != 3:
+                    print("x Invalid URL to get original URL")
+                    print("--- for example: https://www.applau.se/gf")
+                else:
+                    return 'to_unshort'
+            else:
+                return 'to_short'
         else:
             print("x Invalid URL please introduce a valid URL")
-            print("(for example: https://www.example.com/path")
+            print("--- for example: https://www.example.com/path")
             return False
 
     def generate_short_url(self, position):
-        """Generate shorten url based in index"""
-        return self.base_domain + self.combinations[position]
+        """Generate shorten url path based in index"""
+        return ['https://{}'.format(self.base_domain), ''.join(self.combinations[position])]
